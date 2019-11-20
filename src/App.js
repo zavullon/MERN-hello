bd5968d1f41232861bf0c69cfc9deb7cbb35f617
Grigory <MrGriLLla@gmail.com>
2019-11-20 19:13:10 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username : ''
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000')
+            .then(response => {
+                this.setState({
+                    username : response.data.username
+                });
+            })
+            .catch(err => console.log(err));
+        console.log(this.state.username + '!');
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Hello, {this.state.username}!</h3>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,46 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+module.exports = {
+    mode : "development",
+    entry : [
+        "react-hot-loader/patch",
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        __dirname + "/src/index.js"
+    ],
+    output : {
+        path : __dirname + "/public",
+        filename : "bundle.js",
+        publicPath : "/"
+    },
+    module : {
+        rules : [
+            {
+                test : /\.js$/,
+                exclude : /node_modules/,
+                loader : "babel-loader",
+                query : {
+                    presets : ['@babel/preset-env', '@babel/preset-react'],
+                    plugins : ['react-hot-loader/babel', '@babel/plugin-proposal-class-properties']
+
+                }
+            }
+        ]
+    },
+    plugins : [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            inject : true,
+            template : __dirname + "/public/index.html"
+        })
+    ],
+    devServer: {
+        contentBase: './public',
+        historyApiFallback: true,
+        inline: true,
+        hot: true,
+        port: 8080
+    }
+};
